6e12dd00f8bd7251670d38ab6a060c3eb68a06ed
Tai Tram <taittram@yahoo.com>
2015-10-31 18:12:22 -0700

    finished v1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 
 $(document).ready(function(){
+
+  newGame();
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -12,6 +14,66 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+    // new game button
+    $("nav .new").click(function() {
+      newGame();
+    });
+
+    $("section form").submit(function(e) {
+      e.preventDefault();
+      var result = parseInt($("#userGuess").val());
+      var distance = randomNum - result;
+      ++count;
+      $("#userGuess").val("");
+      $("#count").text(count);
+      if (result) {
+        if (result == randomNum) {
+          $("#feedback").text("That's right!");
+          $("#userGuess").hide();
+          $("#guessButton").hide();
+        }
+        else if (distance >= 30) {
+          $("#guessList").append("<li>" + result + "</li>");
+          $("#feedback").text("Colderrr..");
+        }
+        else if (distance >= 20 && distance < 30) {
+          $("#guessList").append("<li>" + result + "</li>");
+          $("#feedback").text("Getting warm!");
+        }
+        else if (distance >= 10 && distance < 20) {
+          $("#guessList").append("<li>" + result + "</li>");
+          $("#feedback").text("Warmer!");
+        }
+        else if (distance >= 1 && distance < 10) {
+          $("#guessList").append("<li>" + result + "</li>");
+          $("#feedback").text("So hot!");
+        }
+        else {
+        $("#guessList").append("<li>" + result + "</li>");
+        $("#feedback").text("Ice cold!");
+        }
+      }
+    });
+
 });
 
+var randomNum = Math.floor(Math.random() * 100) + 1;
 
+var count = 0;
+
+// generate number
+  function makeNumber() {
+    return randomNum;
+  }
+
+// make new game
+  function newGame() {
+    count = 0;
+    randomNum = Math.floor((Math.random() * 100) + 1);
+    $("#guessList li").remove();
+    $("#userGuess").val("");
+    $("#feedback").text("Make your Guess!");
+    $("#count").text(count);
+    $("#userGuess").show();
+    $("#guessButton").show();
+  }
